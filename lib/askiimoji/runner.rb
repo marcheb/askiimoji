6291626e96f9e7815570163f025b209e154a1211
Marc Hébert <crambert@gmail.com>
2013-03-03 22:17:21 -0500

    dancer

--- lib/askiimoji/runner.rb
+++ lib/askiimoji/runner.rb
@@ -7,7 +7,58 @@ module Askiimoji
     end
 
     def run
-      @arguments.map{ |a| Dictionnary.emoji a }.compact.each { |e| puts e.to_s }
+      @arguments.length == 0 ? dancer : @arguments.map{ |a| Dictionnary.emoji a }.compact.each{ |e| puts e.to_s }
+    end
+
+    def dancer
+      3.times do |i|
+        print "\e[1J\e[H"
+
+        print "  (a)  /|\n"
+        print " (_|_)/       <><\n"
+        print "  j|i\n"
+        sleep 0.25
+        print "\e[1J\e[H"
+
+        print "  (s)  /|\n"
+        print " \\_|_)/      <><\n"
+        print "  j|\\ \n"
+        sleep 0.25
+        print "\e[1J\e[H"
+
+        print "  (k)  /|\n"
+        print " (_|_)/     <><\n"
+        print "  /|i\n"
+        sleep 0.25
+        print "\e[1J\e[H"
+
+        print "  (i)  /|\n"
+        print " \\_|_)/    <><\n"
+        print "  j|\\ \n"
+        sleep 0.25
+        print "\e[1J\e[H"
+
+        print "  (i)  /|\n"
+        print " (_|_)/   <><\n"
+        print "  /|i\n"
+        sleep 0.25
+        print "\e[1J\e[H"
+
+        print "  (m)  /|\n"
+        print " \\_|_)/  <><\n"
+        print "  /|\\ \n"
+        sleep 0.25
+        print "\e[1J\e[H"
+
+        print "  (o)  /|\n"
+        print " (_|_)/ <><\n"
+        print "  j|i\n"
+        sleep 0.25
+      end
+      print "\e[1J\e[H"
+      print " w(a)  /|\n"
+      print " |_|_)/ <><\n"
+      print "  j|i\n"
     end
   end
 end
